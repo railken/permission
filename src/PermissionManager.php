a5c17e14ea57f06b35d247a09745559c41a85117
Unknown <railken@protonmail.ch>
2017-10-24 01:19:15 +0200

    add permission manager

---
+++ src/PermissionManager.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Railken\Permission;
+
+
+class PermissionManager
+{
+
+	/**
+	 * Is the permission valid ?
+	 *
+	 * @param string $permission
+	 * @param array $permissions
+	 *
+	 * @return boolean
+	 */
+	public function can($permission, $permissions)
+	{
+		$pp = explode(".", $permission);
+
+		foreach ($permissions as $p) {
+			
+			if ($permission == $p)
+				return true;
+
+			$p = explode(".", $p);
+
+			foreach ($p as $k => $in) {
+
+				if ($in == '*') {
+					return true;
+				}
+
+				if (!isset($pp[$k])) {
+					break;
+				}
+
+				if ($pp[$k] == "*") {
+					return true;
+				}
+
+				if ($pp[$k] != $in) {
+					break;
+				}
+
+
+			}
+
+		}
+
+		return false;
+	}
+} 
