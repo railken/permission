7232fd0e84cccc86ec451e3375d8e03772637f60
Unknown <railken@protonmail.ch>
2017-10-24 01:19:20 +0200

    add can trait

---
+++ src/Traits/CanTrait.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Railken\Permission\Traits;
+
+use Railken\Permission\PermissionManager;
+
+trait CanTrait
+{
+
+	/**
+	 * Permissions
+	 */
+	protected $permissions = [];
+
+	/**
+	 * Set permissions
+	 *
+	 * @param array $permissions
+	 *
+	 * @return $this
+	 */
+	public function setPermissions($permissions)
+	{
+		$this->permissions = $permissions;
+
+		return $this;
+	}
+
+	/**
+	 * Retrieve permissions
+	 *
+	 * @return array
+	 */
+	public function getPermissions()
+	{
+		return $this->permissions;
+	}
+
+	/**
+	 * Can user do this?
+	 *
+	 * param string $permission
+	 *
+	 * @return boolean
+	 */
+	public function can($permission)
+	{
+		$pm = new PermissionManager();
+		return $pm->can($permission, $this->permissions);
+	}
+}
